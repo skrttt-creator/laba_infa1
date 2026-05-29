fde77e0ecf523fb6772302fa2879a951a6d2caec
skrttt-creator <sasha061107@gmail.com>
2026-05-29 14:05:33 +0300

    Create dynamic_array.h

---
+++ dynamic_array.h
@@ -0,0 +1,20 @@
+#ifndef DYNAMIC_ARRAY_H
+#define DYNAMIC_ARRAY_H
+#include "data_types.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+typedef struct {
+    void* data;
+    size_t size;
+    size_t capacity;
+    TypeInf* type;
+} DynamicArray;
+
+DynamicArray* x_mass(TypeInf* type);
+void xfree(DynamicArray* x);
+void xpush(DynamicArray* x, const void* val);
+void xsort(DynamicArray* x, int order);
+// ... (остальные функции: xmap, xwhere, xconcat)
+#endif
