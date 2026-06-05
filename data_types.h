c5d117c51a32f349e05dc773a2ec139d7f8cf55b
skrttt-creator <sasha061107@gmail.com>
2026-06-05 17:46:56 +0300

    Update data_types.h

--- data_types.h
+++ data_types.h
@@ -1,14 +1,17 @@
-#ifndef DATA_TYPES_H
-#define DATA_TYPES_H
-#include <stdio.h>
+#ifndef TYPES_H
+#define TYPES_H
 
-typedef struct {
-    size_t size;
-    int (*comp)(const void*, const void*);
-    void (*print)(const void*);
-} TypeInf;
+#include "dynamic_array.h"
 
-extern TypeInf IntType;
 extern TypeInf DoubleType;
+extern TypeInf StringType;
+
+char* my_strdup(const char* s);
+
+void double_add(void* val, void* context);
+bool double_is_positive(const void* val);
+
+void string_add_suffix(void* val, void* context);
+bool string_len_greater_than_3(const void* val);
+
 #endif
-//
